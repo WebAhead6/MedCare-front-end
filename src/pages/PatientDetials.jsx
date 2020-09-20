1ace8a1232553c5be3ae7c4184bdb2946cf17fa4
mervat <mervat.yosef.14@gmail.com>
2020-09-20 17:20:45 +0300

    profile page create

--- src/pages/PatientDetials.jsx
+++ src/pages/PatientDetials.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import "./patientDetails.css";
 
 const Detials = function () {
   return (
-    <div>
-      <h1>Detials</h1>
-    </div>
+    <main>
+      <div>
+        <h1>Hello User!</h1>
+      </div>
+      <div class="profiledetails">My Profile Details</div>
+      <div class="medicationlist">
+        Medications List
+        <img src="/medicon.svg"></img>
+      </div>
+      <div class="askdoc">
+        Ask a Doctor <img src="/docicon.svg"></img>
+      </div>
+      <div class="medreport">
+        Medical Report <img src="/reporticon.svg"></img>
+      </div>
+    </main>
   );
 };
 
