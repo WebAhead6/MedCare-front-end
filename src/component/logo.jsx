48c655c673a13129666f442008310640578b4887
mervat <mervat.yosef.14@gmail.com>
2020-09-21 15:47:03 +0300

    profilepage create

--- src/component/logo.jsx
+++ src/component/logo.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "../App.css";
 const Logo = function () {
   return (
     <div className="logo">
--- src/pages/PatientDetails.jsx
+++ src/pages/PatientDetails.jsx
@@ -1,22 +1,46 @@
 import React from "react";
 import "./patientDetails.css";
+import Logo from "../component/logo";
+import { BsPersonFill } from "react-icons/bs";
 
 const Detials = function () {
   return (
     <main>
+      <Logo />
       <div>
         <h1>Hello User!</h1>
       </div>
-      <div class="profiledetails">My Profile Details</div>
-      <div class="medicationlist">
-        Medications List
-        <img src="/medicon.svg"></img>
+      <div className="profiledetails">My Profile Details</div>
+      <div className="personalinfo">
+        <span className="profileicon">
+          <BsPersonFill />
+        </span>
+        Patient Name: Abeer <br></br> Age:57 yo<br></br> phone number: 123456789
       </div>
-      <div class="askdoc">
-        Ask a Doctor <img src="/docicon.svg"></img>
+      <div className="container">
+        <button className="medicationlist">
+          Medications List
+          <img className="medicon" src="/medicon.svg"></img>
+        </button>
+        <button className="askdoc">
+          Ask a Doctor <img className="docicon" src="/docicon.svg"></img>
+        </button>
       </div>
-      <div class="medreport">
-        Medical Report <img src="/reporticon.svg"></img>
+      <button className="medreport">
+        Medical Report <img className="reporticon" src="/reporticon.svg"></img>
+      </button>
+
+      <div className="bottomnav">
+        <a href="/patient/details">
+          <img className="homeicon" src="/homeicon.svg"></img>
+        </a>
+        <a href="/patient/medications">
+          <img className="capsule" src="/capsule.svg"></img>
+        </a>
+        <a href="/">
+          {" "}
+          <img className="logout" src="/logouticon.svg"></img>
+        </a>
       </div>
     </main>
   );
